5c73750ce0760cfec27d778c3beffbe40e024c83
Harsh Kumar <harshgbkumar0139@gmail.com>
2023-11-13 19:45:24 +0530

    created home about and contct us page using routing

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './components/Home/Home'
+import Header from './components/header/header'
+import Footer from './components/Footer/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='bg-green-400 p-4' >hii</h1>\
+
+    </>
+  )
+}
+
+export default App
